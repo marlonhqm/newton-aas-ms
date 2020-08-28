682f0f83a097b6b1150134cc0896719fb2941830
Marlon Henrique Queroga Marques <mhqmarques@gmail.com>
2020-08-28 00:36:45 -0300

    alterado arquivo gitignore

---
+++ src/main/java/br/newtonpaiva/tcc/TccManagerApplication.java
@@ -0,0 +1,13 @@
+package br.newtonpaiva.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/newtonpaiva/tcc/TccManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.newtonpaiva.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
